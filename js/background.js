318d02d76588370d518e0950aa53fa1348efc796
NeoMurderer <on@prestoheads.com>
2013-11-16 14:37:42 +0200

    Версия 1.71 Исправлены баги и добавлены новые фильтры

---
+++ js/background.js
@@ -0,0 +1,77 @@
+$(function () {
+	getFilter();
+	getText();
+	$("#selectbox").find("li").click(function () {
+		setFilter($(this));
+	})
+	$(".share a").click(function(event) {
+		var href = $(this).attr("href")
+		window.open(href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');
+		return false;
+	});
+	
+})
+function setFilter(_this) {
+	var elem = _this.attr("id");
+	_this.toggleClass("checked");
+	var type = _this.hasClass("checked");
+	switch (elem) {
+      case "add":
+        chrome.storage.sync.set({"add": type});
+        break;
+      case "apps":
+        chrome.storage.sync.set({"apps": type});
+        break;
+      case "link":
+        chrome.storage.sync.set({"link": type});
+        break;
+      case "vkclean":
+        chrome.storage.sync.set({"vkclean": type});
+        break;
+      case "short_link":
+        chrome.storage.sync.set({"short_link": type});
+        break;
+      case "repost":
+        chrome.storage.sync.set({"repost": type});
+        break;
+    }
+}
+function getFilter () {
+	chrome.storage.sync.get(function(items){
+		console.log(items);
+		if(items.add==true)
+			$("#add").addClass("checked");
+		if(items.apps==true)
+			$("#apps").addClass("checked");
+		if(items.link==true)
+			$("#link").addClass("checked");
+		if(items.vkclean==true)
+			$("#vkclean").addClass("checked");
+		if(items.short_link==true)
+			$("#short_link").addClass("checked");
+		if(items.repost==true)
+			$("#repost").addClass("checked");
+	})
+}
+function getText () {
+	chrome.storage.sync.get("text",function(items){
+		console.log(items);
+		var list = JSON.parse(items.text);
+		for(var i=0,j=list.length;i<j;i++) {
+			$(".text-block").append(
+				$("<li/>",{"text":list[i],"id":i}).prepend(
+					$("<span/>").click(function () {
+						_this = $(this).parent()
+						var text = _this.text();
+						var i = _this.attr("id");
+						list.splice(i, 1);
+						console.log(list);
+						chrome.storage.sync.set({"text": JSON.stringify(list)},function  () {
+							_this.slideUp(500);
+						});
+					})
+				)
+			);
+		}
+	})
+}
---
+++ js/script.js
@@ -0,0 +1,72 @@
+chrome.storage.sync.get('first', function (data) {
+    if (data.first == undefined || data.first === true) {
+        chrome.storage.sync.set({
+            first: false,
+            add: true,
+            apps: true,
+            link: true,
+            vkclean: true,
+            update:true,
+            short_link:true,        
+            repost:false,        
+        })
+    }
+});
+var i = 1;
+chrome.browserAction.onClicked.addListener(function (tab) {
+    chrome.browserAction.setPopup({
+        popup: "popup.html"
+    })
+});
+
+function checkForValidUrl(tabId, changeInfo, tab) {
+    if (changeInfo.status === "loading") {
+        if ((tab.url.indexOf('vk.com') > -1)) {
+
+            chrome.tabs.executeScript(tabId, {
+                file: "js/jquery.js"
+            });
+            chrome.tabs.executeScript(tabId, {
+                file: "js/content_script.js"
+            })
+			console.log("vk")
+        }
+		else
+		{
+			console.log("novk")
+		}
+    }
+};
+chrome.tabs.onUpdated.addListener(checkForValidUrl);
+chrome.tabs.onActivated.addListener(function(activeInfo){
+    chrome.tabs.get(activeInfo.tabId ,function(tab){
+        if ((tab.url.indexOf('vk.com') > -1)) {
+            chrome.contextMenus.create({
+                "id": "block",
+                "title": "Добавить список фильтров",
+                "contexts": ["selection"],
+                "onclick": block
+            });
+        }
+        else {
+            chrome.contextMenus.remove("block")
+        }
+})
+})
+function block(info, tab) {
+    var list = [];
+    chrome.storage.sync.get(function (items) {
+        if (items.text) {
+            list = JSON.parse(items.text);
+            list[list.length] = info.selectionText
+        } else {
+            list[0] = info.selectionText
+        }
+		list.sort();
+        var text = JSON.stringify(list);
+        chrome.storage.sync.set({
+            text: text,
+            update:true
+        })
+    })
+}
